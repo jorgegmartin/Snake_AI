24afadc161fad7e61ac4c09e851fae85d55a0541
j-gmartin <jgmartin199@gmail.com>
2022-06-28 16:06:20 +0200

    developed agent and model, not working properly

--- agent.py
+++ agent.py
@@ -3,19 +3,21 @@ import random
 import numpy as np
 from collections import deque
 from game import SnakeGameAI, Direction, Point
+from model import Linear_QNet, QTrainer
+from helper import plot
 
 MAX_MEMORY = 100_000 # definir memoria para guardar en deque
 BATCH_SIZE = 1000
 LR = 0.001 # learning rate
-torch 
+
 class Agent:
     def __innit__(self):
         self.n_games = 0
         self.epsilon = 0 #randomness
         self.gamma = 0 # discount rate
         self.memory = deque(maxlen=MAX_MEMORY) # popleft() si tenemos demasiados datos ira quitando los de la izquierda
-        self.model = None
-        self.train = None
+        self.model = Linear_QNet(11, 256, 3) # this is the number of neuros in our model, input, hidden, output
+        self.train = QTrainer(self.model, lr=LR, gamma=self.gamma) #gamma is the discoutn rate, which needs to be less than 1
         # to do: model, trainer.
 
 
@@ -67,16 +69,36 @@ class Agent:
 
 
     def remember(self, state, action, reward, next_state, done):
-        self.memory.append(state, action, reward, next_state, done) #popleft if maxmemory is reached
+        self.memory.append((state, action, reward, next_state, done)) #popleft if maxmemory is reached
 
     def train_long(self):
-        pass
+        if len(self.memory) > BATCH_SIZE:
+            mini_sample = random.sample(self.memory, BATCH_SIZE) #list of tuples
+        else:
+            mini_sample = self.memory
+
+        states, actions, rewards, next_states, dones = zip(*mini_sample)
+        self.trainer.train_step(states, actions, rewards, next_states, dones)
 
     def train_short(self, state, action, reward, next_state, done):
-        self.trainer
+        self.trainer.train_step(state, action, reward, next_state, done)
 
     def get_action(self, state):
-        pass
+        #random moves: tradeoff exploration / exploitation
+        self.epsilon = 80 - self.n_games # hardcoded so we can wiggle it
+        final_move = [0, 0, 0]
+        if random.randint(0, 200 < self.epsilon):
+            move = random.randint(0, 2)
+            final_move[move] = 1
+        
+        else:
+            state0 = torch.tensor(state, dtype=torch.float)
+            prediction = self.model(state0)
+            move = torch.argmax(prediction).item()
+            final_move = 1
+        
+        return final_move
+            
 
 def train():
     plot_scores = []
@@ -90,7 +112,7 @@ def train():
         state_old = agent.get_state(game)
 
         # get move
-        final_move = agent.get_actions(state_old)
+        final_move = agent.get_action(state_old)
 
         #perform move and get nw state
         reward, done, score = game.play_step(final_move)
@@ -110,10 +132,16 @@ def train():
 
             if score > record:
                 record = score
-                # agent.model.save()
+                agent.model.save()
             print('Game', agent.n_games, 'Score', 'Record: ', record)
 
-             #TODO: ploting
+            plot_scores.append(scores)
+            total_score += score
+            mean_score = total_score / agent.n_games
+            plot_mean_scores.append(mean_score)
+            plot(plot_scores, plot_mean_scores)
+
+
 
         
 
